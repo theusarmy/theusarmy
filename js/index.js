11b1dfc8396aa466da6c8b4b72e0535bfe8443da
theusarmy <theusarmy.coc@gmail.com>
2017-06-11 22:27:29 -0700

    Create index.js

--- js/index.js
+++ js/index.js
@@ -15,6 +15,6 @@
   
   underlineLinksPointingToThisPage();
   
-  console.log('Finished polishing your page :D (script version 1.2)');
+  console.log('Finished polishing your page :D (script version 1.4)');
   console.log('%cHi there!\n%cYou should seriously join our clan. %c\nThere are web nerds like you in it! \n😉 🇺🇸 %cGo %cArmy!', 'font-size:50px;font-weight:bold;color:red', 'font-size:50px;font-weight:bold;color:blue;','font-size:50px;font-weight:bold;color:red', 'font-size:50px;font-weight:bold;color:blue', 'font-size:50px;font-weight:bold;color:red');
 })();
