ed86af8e63ec300f047c4fbf14fa6059995a6d0f
theusarmy <theusarmy.coc@gmail.com>
2017-06-12 01:25:19 -0700

    Create index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2017 Lin */
 
 (function() {
-  function emboldenLinksPointingToThisPage() {
+  function emboldenAndUnunderlineLinksPointingToThisPage() {
     var links = document.querySelectorAll('a');
     for (var i = 0; i < links.length; i++) {
       var link = links[i];
@@ -9,12 +9,13 @@
       var location = '' + window.location;
       if (href === location || href === location + '#')  {
         link.style.fontWeight = 'bold';
+        link.style.textDecoration = 'none';
       }
     }
   }
   
-  emboldenLinksPointingToThisPage();
+  emboldenAndUnunderlineLinksPointingToThisPage();
   
-  console.log('Finished polishing your page :D (script version 1.6)');
+  console.log('Finished polishing your page :D (script version 1.7)');
   console.log('%cHi there!\n%cYou should seriously join our clan. %c\nThere are web nerds like you in it! \n😉 🇺🇸 %cGo %cArmy!', 'font-size:50px;font-weight:bold;color:red', 'font-size:50px;font-weight:bold;color:blue;','font-size:50px;font-weight:bold;color:red', 'font-size:50px;font-weight:bold;color:blue', 'font-size:50px;font-weight:bold;color:red');
 })();
