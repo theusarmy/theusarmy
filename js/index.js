0877d021539c8a8fddad8fb049c5666a31a31329
theusarmy <theusarmy.coc@gmail.com>
2017-06-11 22:26:03 -0700

    Create index.js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@
   function underlineLinksPointingToThisPage() {
     var links = document.querySelectorAll('a');
     for (var i = 0; i < links.length; i++) {
-      var link = links[i];
-      if (link.href.slice(0, ('' + window.location).length) === ('' + window.location))  {
+      var href = links[i].href;
+      var location = '' + window.location;
+      if (href === location || href === location + '#')  {
         link.style.textDecoration = 'underline';
       }
     }
@@ -13,5 +14,6 @@
   
   underlineLinksPointingToThisPage();
   
-  console.log('Finished polishing your page :D (script version 1.1)');
+  console.log('Finished polishing your page :D (script version 1.2)');
+  console.log('%cHi there!\n%cYou should seriously join our clan. %c\nThere are web nerds like you in it! \n😉 🇺🇸 %cGo %cArmy!', 'font-size:50px;font-weight:bold;color:red', 'font-size:50px;font-weight:bold;color:blue;','font-size:50px;font-weight:bold;color:red', 'font-size:50px;font-weight:bold;color:blue', 'font-size:50px;font-weight:bold;color:red');
 })();
